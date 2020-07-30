8ababfa6484cbc1af212b17a90c083db57e86f64
Gean Carlo Vitorino <gean.carlo.vitorino@gmail.com>
2020-07-30 12:41:42 -0500

    jwt y server creado

---
+++ index.js
@@ -0,0 +1,20 @@
+// index.js
+
+const express = require('express'); 
+const conectarDB = require('./config/db'); 
+
+const app = express(); 
+
+
+conectarDB();
+
+app.use( express.json({ extended: true }));
+
+const PORT = process.env.PORT || 4000; 
+
+app.use('/api/usuarios', require('./routes/usuario'));
+
+
+app.listen(PORT, () => { 
+    console.log(`EL servidor esta funcionando en el puerto ${PORT}`)
+}); 
